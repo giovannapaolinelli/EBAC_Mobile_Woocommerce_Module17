4985810544306c9d5a6ef3b4b85deb8aae28d558
Giovanna <giovanna.paolinelli@gmail.com>
2023-04-15 18:24:47 -0300

    EBAC Mobile Tests Simple Product

--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -1,6 +1,7 @@
 const homeScreens = require("../screens/home.screens");
 const loginScreens = require("../screens/login.screens");
 const myStoreScreens = require("../screens/myStore.screens");
+const simpleProductScreens = require("../screens/simpleProduct.screens");
 
 let urlLoja = 'http://lojaebac.ebaconline.art.br/'
 let usuario = 'gerente'
@@ -19,4 +20,17 @@ describe('Access Admin Panel', () =>{
 
         expect (await myStoreScreens.getStoreName()).toEqual('EBAC - Shop')
     });
+
+describe('Add New Simple Product', () =>{
+        it('should add new simple product', async () => {
+            await simpleProductScreens.clickAddProductMenu()
+            await simpleProductScreens.clickAddProductBtn()
+            await simpleProductScreens.clickAddSimpleProduct()
+            await simpleProductScreens.addProductName("Denim Pants")
+            await simpleProductScreens.addProductValue("80", "50")
+            await simpleProductScreens.publishProduct()
+            expect (await simpleProductScreens.findProductList("Denim Pants")).toBeDisplayed()
+        });
+    });
+    
 })
--- wdio.conf.js
+++ wdio.conf.js
@@ -8,23 +8,11 @@ exports.config = {
     // ====================
     // WebdriverIO supports running e2e tests as well as unit and component tests.
     runner: 'local',
-    
-    //
     // ==================
     // Specify Test Files
     // ==================
     // Define which test specs should run. The pattern is relative to the directory
     // of the configuration file being run.
-    //
-    // The specs are defined as an array of spec files (optionally using wildcards
-    // that will be expanded). The test for each spec file will be run in a separate
-    // worker process. In order to have a group of spec files run in the same worker
-    // process simply enclose them in an array within the specs array.
-    //
-    // If you are calling `wdio` from an NPM script (see https://docs.npmjs.com/cli/run-script),
-    // then the current working directory is where your `package.json` resides, so `wdio`
-    // will be called from there.
-    //
     specs:[
         './test/specs/**/*.js'
     ],
@@ -36,98 +24,42 @@ exports.config = {
     // ============
     // Capabilities
     // ============
-    // Define your capabilities here. WebdriverIO can run multiple capabilities at the same
-    // time. Depending on the number of capabilities, WebdriverIO launches several test
-    // sessions. Within your capabilities you can overwrite the spec and exclude options in
-    // order to group specific specs to a specific capability.
-    //
-    // First, you can define how many instances should be started at the same time. Let's
-    // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
-    // set maxInstances to 1; wdio will spawn 3 processes. Therefore, if you have 10 spec
-    // files and you set maxInstances to 10, all spec files will get tested at the same time
-    // and 30 processes will get spawned. The property handles how many capabilities
-    // from the same test should run tests.
-    //
     maxInstances: 10,
-    //
-    // If you have trouble getting all important capabilities together, check out the
-    // Sauce Labs platform configurator - a great tool to configure your capabilities:
-    // https://saucelabs.com/platform/platform-configurator
-    //
     capabilities: [{
         "platformName": "Android",
-        "appium:platformVersion": "11.0",
-        "appium:deviceName": "EBAC-QA",
-        "appium:automationName": "UiAutomator2",
-        "appium:appWaitActivity": ".ui.login.LoginActivity",
-        "appium:appPackage": "com.woocommerce.android",
-        "appium:appActivity": ".ui.main.MainActivity"
+        // "project": "EBAC-WooCommerce",
+        // "build": "browserstack-build-1",
+        // "name": "local_test",
+        // "device": "Google Pixel 3",
+        // "os_version": "9.0",
+        // "app": 'bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c'
+       "appium:platformVersion": "11.0",
+       "appium:deviceName": "EBAC-QA",
+       "appium:automationName": "UiAutomator2",
+       "appium:appWaitActivity": ".ui.login.LoginActivity",
+       "appium:appPackage": "com.woocommerce.android",
+       "appium:appActivity": ".ui.main.MainActivity",
+       "appium:autoGrantPermissions": true
     }],
     //
     // ===================
     // Test Configurations
     // ===================
     // Define all options that are relevant for the WebdriverIO instance here
-    //
-    // Level of logging verbosity: trace | debug | info | warn | error | silent
     logLevel: 'info',
-    //
-    // Set specific log levels per logger
-    // loggers:
-    // - webdriver, webdriverio
-    // - @wdio/browserstack-service, @wdio/devtools-service, @wdio/sauce-service
-    // - @wdio/mocha-framework, @wdio/jasmine-framework
-    // - @wdio/local-runner
-    // - @wdio/sumologic-reporter
-    // - @wdio/cli, @wdio/config, @wdio/utils
-    // Level of logging verbosity: trace | debug | info | warn | error | silent
-    // logLevels: {
-    //     webdriver: 'info',
-    //     '@wdio/appium-service': 'info'
-    // },
-    //
-    // If you only want to run your tests until a specific amount of tests have failed use
-    // bail (default is 0 - don't bail, run all tests).
     bail: 0,
-    //
-    // Set a base URL in order to shorten url command calls. If your `url` parameter starts
-    // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
-    // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
-    // gets prepended directly.
     baseUrl: 'http://localhost',
     port: 4723,
+    // user: process.env.BROWSERSTACK_USERNAME || 'giovannashinhe_r0QjYc',
+    // key: process.env.BROWSERSTACK_ACCESS_KEY || 'gNoJ2z35YoYxKHnsTs1A',
+    // services: ['browserstack'],
     path: '/wd/hub',
     // Default timeout for all waitFor* commands.
     waitforTimeout: 100000,
-    //
-    // Default timeout in milliseconds for request
-    // if browser driver or grid doesn't send response
     connectionRetryTimeout: 120000,
-    //
-    // Default request retries count
     connectionRetryCount: 3,
-    //
-    // Test runner services
-    // Services take over a specific job you don't want to take care of. They enhance
-    // your test setup with almost no effort. Unlike plugins, they don't add new
-    // commands. Instead, they hook themselves up into the test process.
-    
-    // Framework you want to run your specs with.
-    // The following are supported: Mocha, Jasmine, and Cucumber
-    // see also: https://webdriver.io/docs/frameworks
-    //
-    // Make sure you have the wdio adapter package for the specific framework installed
-    // before running any tests.
     framework: 'mocha',
-    //
-    // The number of times to retry the entire specfile when it fails as a whole
-    // specFileRetries: 1,
-    //
-    // Delay in seconds between the spec file retry attempts
-    // specFileRetriesDelay: 0,
-    //
-    // Whether or not retried specfiles should be retried immediately or deferred to the end of the queue
-    // specFileRetriesDeferred: false,
+
      reporters: ['spec',
         ['allure', {
         outputDir: 'allure-results',
